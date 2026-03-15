e57b71b6626be36cca8c6cf3409f10b79eb76ee8
susmik <155214053+suun034@users.noreply.github.com>
2026-03-15 23:56:52 +0530

    Add files via upload
    more to be added : first bunch For now

---
+++ liveBlogLinks.js
@@ -0,0 +1,12 @@
+const linkBlogs = {
+    "JavaScript Arrays 101" : "https://understandingjavascript.hashnode.dev/javascript-arrays-101",
+    "Array Methods You Must Know" : "https://understandingjavascript.hashnode.dev/array-methods-you-must-know",
+    "Arrow Functions in JavaScript: A Simpler Way to Write Functions" : "https://understandingjavascript.hashnode.dev/arrow-functions-in-javascript-a-simpler-way-to-write-functions",
+    "Control Flow in JavaScript: If, Else, and Switch Explained" : "https://understandingjavascript.hashnode.dev/control-flow-in-javascript-if-else-and-switch-explained",
+    "Understanding Variables and Data Types in JavaScript" : "https://understandingjavascript.hashnode.dev/understanding-variables-and-data-types-in-javascript",
+    "Understanding Objects in JavaScript" : "https://understandingjavascript.hashnode.dev/understanding-objects-in-javascript",
+    "Understanding Object-Oriented Programming in JavaScript" : "https://understandingjavascript.hashnode.dev/understanding-object-oriented-programming-in-javascript",
+    "The Magic of this, call(), apply(), and bind() in JavaScript" : "https://understandingjavascript.hashnode.dev/the-magic-of-this-call-apply-and-bind-in-javascript",
+    "JavaScript Operators: The Basics You Need to Know" : "https://understandingjavascript.hashnode.dev/javascript-operators-the-basics-you-need-to-know",
+    "Function Declaration vs Function Expression: What’s the Difference?" : "https://understandingjavascript.hashnode.dev/function-declaration-vs-function-expression-what-s-the-difference"
+}
