4125d068b3767e96111415b75f2c19271c1c39b6
susmik <155214053+suun034@users.noreply.github.com>
2026-03-16 01:41:53 +0530

    add missed blog links for JavaScript topics

--- liveBlogLinks.js
+++ liveBlogLinks.js
@@ -10,3 +10,20 @@ const linkBlogs = {
     "JavaScript Operators: The Basics You Need to Know" : "https://understandingjavascript.hashnode.dev/javascript-operators-the-basics-you-need-to-know",
     "Function Declaration vs Function Expression: What’s the Difference?" : "https://understandingjavascript.hashnode.dev/function-declaration-vs-function-expression-what-s-the-difference"
 }
+
+//these are less of me more than machine so i know i need to study them hard
+const missedOnes = {
+    "Emmet for HTML: A Beginner’s Guide to Writing Faster Markup" : "https://understandingjavascript.hashnode.dev/emmet-for-html-a-beginner-s-guide-to-writing-faster-markup",
+    "CSS Selectors 101: Targeting Elements with Precision" : "https://understandingjavascript.hashnode.dev/css-selectors-101-targeting-elements-with-precision",
+    "Understanding HTML Tags and Elements" : "https://understandingjavascript.hashnode.dev/understanding-html-tags-and-elements",
+    "How a Browser Works: A Beginner-Friendly Guide to Browser Internals" : "https://understandingjavascript.hashnode.dev/how-a-browser-works-a-beginner-friendly-guide-to-browser-internals",
+    "Understanding Network Devices" : "https://understandingjavascript.hashnode.dev/understanding-network-devices",
+    "How DNS Resolution works?" : "https://understandingjavascript.hashnode.dev/how-dns-resolution-works",
+    "TCP Working: 3-Way Handshake & Reliable Communication" : "https://understandingjavascript.hashnode.dev/tcp-working-3-way-handshake-reliable-communication",
+    "Getting Started with cURL" : "https://understandingjavascript.hashnode.dev/getting-started-with-curl",
+    "DNS Record Types Explained" : "https://understandingjavascript.hashnode.dev/dns-record-types-explained",
+    "TCP vs UDP: When to Use What, and How TCP Relates to HTTP" : "https://understandingjavascript.hashnode.dev/tcp-vs-udp-when-to-use-what-and-how-tcp-relates-to-http",
+    "Why Version Control Exists: The Pendrive Problem" : "https://understandingjavascript.hashnode.dev/why-version-control-exists-the-pendrive-problem",
+    "Git for Beginners: Basics and Essential Commands" : "https://understandingjavascript.hashnode.dev/git-for-beginners-basics-and-essential-commands",
+    "Inside Git: How It Works and the Role of the .git Folder" : "https://understandingjavascript.hashnode.dev/inside-git-how-it-works-and-the-role-of-the-git-folder"
+}
